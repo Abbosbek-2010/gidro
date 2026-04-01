e71baa7e3e2a7e1bb3ccd2eed1c1157550fb41b7
Abbosbek <your_email@example.com>
2026-04-01 16:27:54 +0500

    ishladi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Categories from './pages/Abbosbek/Categories'
+import Add from './pages/Abdulloh/Add'
+import Products from './pages/Abdulloh/Products'
+import Dashboard from './pages/Sulaymon/Dashboard'
+
 
 function App() {
 
   return (
     <>
-      <h1>Gidrooo</h1>
+      <div className='body'>
+        <div className='body_text'>
+          <h3>Dashboard</h3>
+          <h3>Products</h3>
+          <h3>Categories</h3>
+        </div>
+        <div>
+          <hr />
+        </div>
+      </div>
      <Routes>
-       <Route path='' element={<S/>}/>
+       <Route path='categories' element={<Categories/>}/>
+       <Route path='add' element={<Add/>}/>
+       <Route path='products' element={<Products/>}/>
+       <Route path='dashboard' element={<Dashboard/>}/>
      </Routes>
     </>
   )
